2555d2529485b299c2dde05925046aec01f28438
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-02-16 18:55:15 +0000

    Task manager refactored, some styling.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -16,26 +16,25 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
-
 
   private
 
   def task_params
     params.require(:task).permit(:name, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
